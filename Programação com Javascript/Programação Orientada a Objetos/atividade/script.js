a6e93013663ea5037626dbf335d093d4fcf136cd
Emy-Liberato <emilly.lliberato@gmail.com>
2025-04-18 16:22:15 -0300

    módulo programação orientada a objetos

---
+++ Programação com Javascript/Programação Orientada a Objetos/atividade/script.js
@@ -0,0 +1,34 @@
+let resultado = document.getElementById("resultado");
+
+class Parquimetro {
+  constructor(valor) {
+    this.valor = valor;
+  }
+
+  calcularTempo() {
+    if (this.valor < 1) {
+      resultado.innerHTML = "Valor insuficiente";
+    } else if (this.valor >= 3) {
+      let calculo = this.valor - 3
+        resultado.innerHTML = `Você tem 120 minutos para utilizar, e seu troco é de ${
+        calculo.toFixed(2)
+      }`;
+    } else if (this.valor >= 1.75) {
+        let calculo = this.valor - 1.75;
+      resultado.innerHTML = `Você tem 60 minutos para utilizar, e seu troco é de ${
+        calculo.toFixed(2)
+      }`;
+    } else if (this.valor >= 1) {
+        let calculo = this.valor - 1;
+      resultado.innerHTML = `Você tem 30 minutos para utilizar, e seu troco é de ${
+        calculo.toFixed(2)
+      }`;
+    }
+  }
+}
+
+function instanciarObjeto() {
+  let valorBruto = parseFloat(document.getElementById("valor").value);
+  let parquimetro = new Parquimetro(valorBruto);
+  parquimetro.calcularTempo();
+}
---
+++ Programação com Javascript/Programação Orientada a Objetos/exemplo/exemplo.js
@@ -0,0 +1,58 @@
+// objeto literal
+const pessoa = { nome: "Carlos", idade: 20 };
+
+// criar classe
+class Veiculo {
+  // método construtor
+  constructor(marca, modelo, ano) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+
+    // Atributo privado para indicar se o veículo está ligado
+    this._ligado = false;
+  }
+
+  // metodos
+  ligar() {
+    this._ligado = true;
+    console.log("O veículo está ligado");
+  }
+  desligar() {
+    this._ligado = false;
+    console.log("O veículo está desligado");
+  }
+
+  // Metodo getter para obter valor do atributo
+  get ligado() {
+    return this._ligado;
+  }
+}
+
+// herança
+class Moto extends Veiculo {
+  constructor(marca, modelo, ano) {
+    super(marca, modelo, ano);
+  }
+}
+
+class Carro extends Veiculo {
+  constructor(marca, modelo, ano, numeroPostas) {
+    super(marca, modelo, ano)
+    this.numeroPostas = numeroPostas;
+  }
+
+  abrirPortas() {
+    console.log("As postas do carro foram abertas.");
+  }
+}
+
+const carro = new Carro("Honda", "Civic", 2025, 4);
+const moto = new Moto("Yamaha", "MT-07", 2025); 
+
+carro.ligar();
+carro.desligar();
+carro.abrirPortas();
+
+console.log("O carro está ligado?", carro.ligado);
+
